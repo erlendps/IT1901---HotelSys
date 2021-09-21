10ce75a7ea56431a08df14cfe9222c31aa024123
Magne Erlendsønn Tenstad <magneet@stud.ntnu.no>
2021-09-21 11:03:04 +0000

    Add ComponentTemplate

---
+++ gr2116-project/ui/src/main/java/gr2116/ui/components/ComponentTemplate.java
@@ -0,0 +1,16 @@
+package gr2116.ui.components;
+import gr2116.ui.utils.FXMLUtils;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class ComponentTemplate extends VBox { // Must extend a javafx class
+
+    public ComponentTemplate() {
+        FXMLUtils.loadFXML(this); // Automatically connects to ComponentTemplate.fxml, the END of the constructor
+    }
+    
+    @FXML
+    private void initialize() { // This will run after FXMLUtils.loadFXML(this);
+        
+    } 
+}
