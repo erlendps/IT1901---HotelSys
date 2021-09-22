14f7635bc8c309944d5bbc8debda98435075dcc7
magnetenstad <tenstad.magne@gmail.com>
2021-09-23 01:18:03 +0200

    Add descriptions to HotelRoomType

--- gr2116-project/core/src/main/java/gr2116/core/HotelRoomType.java
+++ gr2116-project/core/src/main/java/gr2116/core/HotelRoomType.java
@@ -1,19 +1,30 @@
 package gr2116.core;
+
 public enum HotelRoomType {
-	Single("Enkeltrom"),
-	Double("Dobbeltrom"),
-	Triple("Tremannsrom"),
-	Quad("Firmannsrom"),
-	Suite("Suite"), //Er det noe bedre?
-	Penthouse("Toppleilighet"); // Er det noe bedre?
+	Single("Enkeltrom", "Et rom for én."),
+	Double("Dobbeltrom", "Et rom for to."),
+	Triple("Tremannsrom", "Et rom for tre."),
+	Quad("Firmannsrom", "Et rom for fire."),
+	Suite("Suite", "En romslig suite."), //Er det noe bedre?
+	Penthouse("Toppleilighet", "En toppleilighet med vakker utsikt."); // Er det noe bedre?
 	
+	private final String name;
 	private final String description;
 
-	HotelRoomType(String description) {
+	HotelRoomType(String name, String description) {
+		this.name = name;
 		this.description = description;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 	public String getDescription() {
-		return this.description;
+		return description;
+	}
+
+	public String toString() {
+		return getName();
 	}
 }
