a7a241a59ac4f88cd33013a69fbebfe0ef3f5a91
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-09-23 14:45:13 +0000

    Changed HotelRoomType description to english.

--- gr2116-project/core/src/main/java/gr2116/core/HotelRoomType.java
+++ gr2116-project/core/src/main/java/gr2116/core/HotelRoomType.java
@@ -1,12 +1,12 @@
 package gr2116.core;
 
 public enum HotelRoomType {
-	Single("Enkeltrom", "Et rom for én."),
-	Double("Dobbeltrom", "Et rom for to."),
-	Triple("Tremannsrom", "Et rom for tre."),
-	Quad("Firmannsrom", "Et rom for fire."),
-	Suite("Suite", "En romslig suite."), //Er det noe bedre?
-	Penthouse("Toppleilighet", "En toppleilighet med vakker utsikt."); // Er det noe bedre?
+	Single("Single room", "A room for one."),
+	Double("Double room", "A room for two"),
+	Triple("Tripple room", "A  room for three."),
+	Quad("Qaud room", "A room for four."),
+	Suite("Suite", "Separate living area and bedroom."), //Er det noe bedre?
+	Penthouse("Penthouse", "A penthouse with a beautiful view."); // Er det noe bedre?
 	
 	private final String name;
 	private final String description;
