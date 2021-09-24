c19f72da7f046e1f68462bc007fd06d1790e68a8
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-09-24 08:39:23 +0000

    Added text to inform user if wrong

--- gr2116-project/ui/src/main/java/gr2116/ui/pages/MainPage.java
+++ gr2116-project/ui/src/main/java/gr2116/ui/pages/MainPage.java
@@ -3,6 +3,7 @@ import gr2116.ui.utils.FXMLUtils;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -16,6 +17,7 @@ import gr2116.ui.message.MessageListener;
 import gr2116.ui.components.FilterPanel;
 import gr2116.ui.components.HotelRoomFilter;
 import gr2116.ui.components.HotelRoomListItem;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class MainPage extends VBox implements MessageListener {
@@ -55,16 +57,34 @@ public class MainPage extends VBox implements MessageListener {
     }
     
     private void buildRoomList() {
+        roomItemContainer.getChildren().clear();
         if (hotelRoomFilter == null) {
             return;
         }
         if (!hotelRoomFilter.isValid()) {
+            HotelRoomType roomType = hotelRoomFilter.getRoomType();
+            LocalDate startDate = hotelRoomFilter.getStartDate();
+            LocalDate endDate = hotelRoomFilter.getEndDate();
+            
+            if( roomType == null){
+                Label label = new Label("No room type is set yet.");
+                roomItemContainer.getChildren().add(label);
+            }
+            if( startDate == null || endDate == null ){
+                Label label = new Label("No dates are picked yet.");
+                roomItemContainer.getChildren().add(label);
+            }
+            if( endDate.isBefore(startDate) ){
+                Label label = new Label("The end date picked is before the start date.");
+                roomItemContainer.getChildren().add(label);
+            }
+        
             return; // TODO: Inform the user about why the filter is invalid.
         }
         
         Collection<HotelRoom> filteredRooms = hotel.getRooms(hotelRoomFilter.getPredicate());
 
-        roomItemContainer.getChildren().clear();
+        
         for (HotelRoom hotelRoom : filteredRooms) {
             HotelRoomListItem roomItem = new HotelRoomListItem(hotelRoom);
             roomItem.setOnMakeReservationButtonAction((event) -> {
