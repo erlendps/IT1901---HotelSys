0352f98f0a2f86969d6c4a17496c351c4f1220b5
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2021-09-24 11:13:44 +0200

    Updated person test

--- gr2116-project/core/src/test/java/gr2116/core/PersonTest.java
+++ gr2116-project/core/src/test/java/gr2116/core/PersonTest.java
@@ -1,15 +1,20 @@
 package gr2116.core;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
     Person person;
+    Person tom;
 
     @BeforeEach
     public void setup() {
         person = new Person("Mr. Game and Watch");
+        tom = new Person("Tom Haddleford");
+        tom.addBalance(1000);
     }
 
     @Test
@@ -31,5 +36,15 @@ public class PersonTest {
         });
         person.setEmail("yolo.noob@noob.com");
         assertEquals("yolo.noob@noob.com", person.getEmail());
+        tom.setEmail("tom@richpeople.com");
+        assertEquals("tom@richpeople.com", person.getEmail());
+    }
+
+    @Test
+    public void testPay() {
+        HotelRoom room = new HotelRoom(HotelRoomType.Double, 100);
+        room.setPrice(100);
+        tom.makeReservation(room, LocalDate.of(2021, 7, 6), LocalDate.of(2021, 7, 7));
+        assertEquals(900, tom.getBalance());
     }
 }
