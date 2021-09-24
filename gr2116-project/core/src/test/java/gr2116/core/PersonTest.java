43c367566b1dfd49d4f5dede4462dedde12249ca
Erlend Skaaden <erlendskaaden@gmail.com>
2021-09-24 11:41:24 +0200

    changed the import so it imports all assertions

--- gr2116-project/core/src/test/java/gr2116/core/PersonTest.java
+++ gr2116-project/core/src/test/java/gr2116/core/PersonTest.java
@@ -1,6 +1,5 @@
 package gr2116.core;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
