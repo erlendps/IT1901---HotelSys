8a1fef7594c9bc5f6e46cf48a3dc9a70fcb6a0dc
Magne Erlendsønn Tenstad <magneet@stud.ntnu.no>
2021-10-05 08:22:11 +0000

    Add search room by amenities #30

--- gr2116-project/ui/src/main/java/gr2116/ui/components/FilterPanel.java
+++ gr2116-project/ui/src/main/java/gr2116/ui/components/FilterPanel.java
@@ -1,19 +1,25 @@
 package gr2116.ui.components;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+
+import gr2116.core.Amenity;
 import gr2116.core.HotelRoomType;
 import gr2116.ui.message.Message;
 import gr2116.ui.message.MessageListener;
 import gr2116.ui.utils.FXMLUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class FilterPanel extends VBox {
 	Collection<MessageListener> listeners = new HashSet<>();
+	HashMap<Amenity, Boolean> amenities = new HashMap<>();
 
 	@FXML
 	private DatePicker startDatePicker, endDatePicker;
@@ -23,6 +29,9 @@ public class FilterPanel extends VBox {
 
 	@FXML
 	private Label roomTypeDescription;
+
+	@FXML
+	private VBox amenitiesContainer;
 
     public FilterPanel() {
         FXMLUtils.loadFXML(this);
@@ -34,6 +43,19 @@ public class FilterPanel extends VBox {
 
 		for (HotelRoomType roomType : HotelRoomType.values()) {
 			roomTypeChoiceBox.getItems().add(roomType);
+		}
+		for (Amenity amenity : Amenity.values()) {
+			amenities.put(amenity, false);
+			HBox hBox = new HBox();
+			CheckBox checkBox = new CheckBox();
+			Label label = new Label(amenity.getName());
+			checkBox.setOnAction((event) -> {
+				amenities.put(amenity, checkBox.isSelected());
+				notifyListeners();
+			});
+			hBox.getChildren().add(checkBox);
+			hBox.getChildren().add(label);
+			amenitiesContainer.getChildren().add(hBox);
 		}
 		
 		startDatePicker.setOnAction((event) -> {
@@ -61,7 +83,7 @@ public class FilterPanel extends VBox {
 	}
 	public void notifyListeners() {
 		for (MessageListener listener : listeners) {
-			HotelRoomFilter filter = new HotelRoomFilter(startDatePicker.getValue(), endDatePicker.getValue(), roomTypeChoiceBox.getValue());
+			HotelRoomFilter filter = new HotelRoomFilter(startDatePicker.getValue(), endDatePicker.getValue(), roomTypeChoiceBox.getValue(), amenities);
 			listener.receiveNotification(this, Message.Filter, filter);
 		}
 	}
--- gr2116-project/ui/src/main/java/gr2116/ui/components/HotelRoomFilter.java
+++ gr2116-project/ui/src/main/java/gr2116/ui/components/HotelRoomFilter.java
@@ -1,8 +1,10 @@
 package gr2116.ui.components;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.function.Predicate;
 
+import gr2116.core.Amenity;
 import gr2116.core.HotelRoom;
 import gr2116.core.HotelRoomType;
 
@@ -10,11 +12,13 @@ public class HotelRoomFilter {
 	private final LocalDate startDate;
 	private final LocalDate endDate;
 	private final HotelRoomType roomType;
+	private final HashMap<Amenity, Boolean> amenities;
 
-	public HotelRoomFilter(LocalDate startDate, LocalDate endDate, HotelRoomType roomType) {
+	public HotelRoomFilter(LocalDate startDate, LocalDate endDate, HotelRoomType roomType, HashMap<Amenity, Boolean> amenities) {
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.roomType = roomType;
+		this.amenities = amenities;
 	}
 	
 	public boolean isValid() {
@@ -25,6 +29,9 @@ public class HotelRoomFilter {
 			return false;
 		}
 		if (endDate.isBefore(startDate)) {
+			return false;
+		}
+		if (amenities == null) {
 			return false;
 		}
 		return true;
@@ -41,10 +48,15 @@ public class HotelRoomFilter {
 			if (room.getRoomType() != roomType) {
 				return false;
 			}
+			for (Amenity amenity : amenities.keySet()) {
+				if (amenities.get(amenity) && !room.hasAmenity(amenity)) {
+					return false;
+				}
+			}
 			return true;
 		};
 	}
-
+	
 	public LocalDate getStartDate() {
 		return startDate;
 	}
