9f3b51f9d5d465521a78f6d86892faee4406dcd0
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-10-05 09:06:18 +0000

    Dependicy to MockitoExtencion added #29

--- gr2116-project/core/src/test/java/gr2116/core/HotelRoomTest.java
+++ gr2116-project/core/src/test/java/gr2116/core/HotelRoomTest.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)  
 public class HotelRoomTest {
