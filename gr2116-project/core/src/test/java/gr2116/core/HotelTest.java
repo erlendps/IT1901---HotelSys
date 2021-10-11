f63a393bcdc51b24e5fb5b8f14faf8bc758e4c2d
Erlend Skaaden <erlendskaaden@gmail.com>
2021-10-11 16:02:02 +0200

    finished test for Hotel class #37

---
+++ gr2116-project/core/src/test/java/gr2116/core/HotelTest.java
@@ -0,0 +1,80 @@
+package gr2116.core;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Arrays;
+
+/**
+ * Test class for Hotel class
+ */
+public class HotelTest {
+    Hotel hotel;
+    HotelRoom room1 = mock(HotelRoom.class);
+    HotelRoom room2 = mock(HotelRoom.class);
+    Collection<HotelRoom> rooms = new ArrayList<>(Arrays.asList(room1, room2));
+
+    @BeforeEach
+    public void setup() {
+        hotel = new Hotel();
+    }
+
+    @Test
+    public void testConstructor() {
+        Hotel emptyHotel = new Hotel();
+        assertEquals(Arrays.asList(), emptyHotel.getRooms());
+        Hotel fullHotel = new Hotel(rooms);
+        assertEquals(rooms, fullHotel.getRooms());
+    }
+
+    @Test
+    public void testRoomsIsPersistent() {
+        Hotel fullHotel = new Hotel(rooms);
+        HotelRoom room3 = mock(HotelRoom.class);
+        rooms.add(room3);
+        assertNotEquals(rooms, fullHotel.getRooms());
+    }
+
+    @Test
+    public void testAddRoom() {
+        assertEquals(Arrays.asList(), hotel.getRooms());
+        hotel.addRoom(room1);
+        hotel.addRoom(room2);
+        assertEquals(Arrays.asList(room1, room2), hotel.getRooms());
+        assertThrows(NullPointerException.class, () -> hotel.addRoom(null));
+    }
+
+    @Test
+    public void testRemoveRoom() {
+        assertThrows(IllegalArgumentException.class, () -> hotel.removeRoom(room1));
+        hotel.addRoom(room1);
+        hotel.removeRoom(room1);
+        assertEquals(Arrays.asList(), hotel.getRooms());
+    }
+
+    @Test
+    public void testGetRoomsPredicate() {
+        HotelRoom room3 = mock(HotelRoom.class);
+        when(room1.getRoomType()).thenReturn(HotelRoomType.Single);
+        when(room2.getRoomType()).thenReturn(HotelRoomType.Double);
+        hotel.addRoom(room1);
+        hotel.addRoom(room2);
+        hotel.addRoom(room3);
+        assertEquals(Arrays.asList(room1), hotel.getRooms((room) -> room.getRoomType() == HotelRoomType.Single));
+        when(room1.getNumber()).thenReturn(2);
+        when(room2.getNumber()).thenReturn(5);
+        when(room3.getNumber()).thenReturn(7);
+        assertEquals(Arrays.asList(room2, room3), hotel.getRooms((room) -> room.getNumber() > 3));
+        
+        when(room1.hasAmenity(Amenity.Internet)).thenReturn(true);
+        when(room2.hasAmenity(Amenity.Internet)).thenReturn(false);
+        assertEquals(Arrays.asList(room1), hotel.getRooms((room) -> room.hasAmenity(Amenity.Internet)));
+    }
+}
