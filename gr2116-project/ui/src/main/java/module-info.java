a1dd6f0df30e5241b42dfd9c5b48fc4136f13bda
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2021-10-14 11:49:16 +0200

    Setup TestFX and make example test #56

--- gr2116-project/ui/src/main/java/module-info.java
+++ gr2116-project/ui/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module gr2116.ui {
+
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+
     requires gr2116.core;
     requires gr2116.persistence;
+    
     opens gr2116.ui.app to javafx.graphics;
     opens gr2116.ui.controller to javafx.fxml;
     opens gr2116.ui.components to javafx.fxml;
--- gr2116-project/ui/src/test/java/gr2116/ui/AppTest.java
+++ gr2116-project/ui/src/test/java/gr2116/ui/AppTest.java
@@ -1,2 +1,41 @@
+package gr2116.ui;
 
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Application, the booking system application.
+ */
+
+public class AppTest extends ApplicationTest{
+  /**
+   * Start the app, load FXML and show scene.
+   * @throws IOException
+   */
+
+  @Start
+  public void start(Stage stage) throws IOException {
+    Parent parent = FXMLLoader.load(
+      getClass().getClassLoader().getResource("App.fxml"));
+  stage.setScene(new Scene(parent));
+  stage.show();
+  }
+  
+  @Test
+  void enterUserText() {
+    FxAssert.verifyThat("#signInButton", LabeledMatchers.hasText("Sign in"));
+  }
+}
