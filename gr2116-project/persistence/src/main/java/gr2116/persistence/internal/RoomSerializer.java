5f802882443b79ee593a875544514e63afc2066b
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-02 17:06:48 +0100

    #98 add (de)serializer for HotelRoom class

--- gr2116-project/persistence/src/main/java/gr2116/persistence/internal/RoomSerializer.java
+++ gr2116-project/persistence/src/main/java/gr2116/persistence/internal/RoomSerializer.java
@@ -1,5 +1,50 @@
 package gr2116.persistence.internal;
 
-public class RoomSerializer {
-    
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import gr2116.core.HotelRoom;
+import gr2116.core.Reservation;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Serializer for HotelRoom class.
+ */
+public class RoomSerializer extends JsonSerializer<HotelRoom> {
+
+  /*
+  format:
+  {
+    "number": ... ,
+    "type": "...",
+    "amenities": [ ... ],
+    "price": ... ,
+    "reservations": [ ... ]
+  }
+  */
+
+  @Override
+  public void serialize(HotelRoom room, JsonGenerator gen,
+      SerializerProvider serializers) throws IOException {
+    if (room != null) {
+      gen.writeStartObject();
+      gen.writeNumberField("number", room.getNumber());
+      gen.writeStringField("type", room.getRoomType().name());
+      gen.writeArrayFieldStart("amenities");
+      for (String amenity : room.getAmenities()) {
+        gen.writeString(amenity);
+      }
+      gen.writeEndArray();
+      gen.writeNumberField("price", room.getPrice());
+      gen.writeArrayFieldStart("reservations");
+      Iterator<Reservation> it = room.getCalendar().iterator();
+      while (it.hasNext()) {
+        gen.writeObject(it.next());
+      }
+      gen.writeEndArray();
+    }
+  }
 }
