0e55f3b014ee600b5db4addd73c3dd72b96ed853
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-02 10:49:51 +0100

    #97 removed empty line in module info

--- gr2116-project/RESTservice/src/main/java/module-info.java
+++ gr2116-project/RESTservice/src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module gr2116.RESTservice {
-<<<<<<< HEAD
-    
-=======
     requires jakarta.ws.rs;
 
     requires jersey.common;
@@ -15,6 +12,4 @@ module gr2116.RESTservice {
     requires gr2116.persistence;
 
     opens gr2116.restapi to jersey.server;
-
->>>>>>> b690c6f (#97 fixed module-info to work with dependencies, commented spotbugs since it failed when no files are present)
 }
