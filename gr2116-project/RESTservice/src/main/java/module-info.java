1259ed3ed34d8f191b976144a531bb9fe1efdda6
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-02 10:46:37 +0100

    fixed a merge conflict #97

--- gr2116-project/RESTservice/src/main/java/module-info.java
+++ gr2116-project/RESTservice/src/main/java/module-info.java
@@ -1,3 +1,20 @@
 module gr2116.RESTservice {
+<<<<<<< HEAD
     
+=======
+    requires jakarta.ws.rs;
+
+    requires jersey.common;
+    requires jersey.server;
+    requires jersey.media.json.jackson;
+
+    requires org.glassfish.hk2.api;
+    requires org.slf4j;
+
+    requires gr2116.core;
+    requires gr2116.persistence;
+
+    opens gr2116.restapi to jersey.server;
+
+>>>>>>> b690c6f (#97 fixed module-info to work with dependencies, commented spotbugs since it failed when no files are present)
 }
