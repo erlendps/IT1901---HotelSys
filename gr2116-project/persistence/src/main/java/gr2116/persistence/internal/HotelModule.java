b44d719fab52369cfcadd5378aa26858f56458ca
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-02 17:05:26 +0100

    #98 add a custom Jackson module

--- gr2116-project/persistence/src/main/java/gr2116/persistence/internal/HotelModule.java
+++ gr2116-project/persistence/src/main/java/gr2116/persistence/internal/HotelModule.java
@@ -1,5 +1,35 @@
 package gr2116.persistence.internal;
 
-public class HotelModule {
-    
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import gr2116.core.HotelRoom;
+import gr2116.core.Person;
+import gr2116.core.Reservation;
+
+/**
+ * Custom Jackson module that extends SimpleModule.
+ */
+public class HotelModule extends SimpleModule {
+  
+  private static final String NAME = "HotelModule";
+
+  /**
+   * Constructor of HotelModule that extends the Jackson SimpleModule.
+   */
+  public HotelModule() {
+    super(NAME, Version.unknownVersion());
+
+    // Person
+    addSerializer(Person.class, new PersonSerializer());
+    addDeserializer(Person.class, new PersonDeserializer());
+
+    // Reservation
+    addSerializer(Reservation.class, new ReservationSerializer());
+    addDeserializer(Reservation.class, new ReservationDeserializer());
+
+    // HotelRoom
+    addSerializer(HotelRoom.class, new RoomSerializer());
+    addDeserializer(HotelRoom.class, new RoomDeserializer());
+  }
 }
