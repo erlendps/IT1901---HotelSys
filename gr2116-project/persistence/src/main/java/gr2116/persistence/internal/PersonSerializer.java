f5807a35dd15020601f7123d6ebd94ef4d54742f
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-02 13:09:52 +0100

    #98 add (de)serializer classes

---
+++ gr2116-project/persistence/src/main/java/gr2116/persistence/internal/PersonSerializer.java
@@ -0,0 +1,13 @@
+package gr2116.persistence.internal;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+
+import gr2116.core.Person;
+
+
+public class PersonSerializer {
+    
+}
