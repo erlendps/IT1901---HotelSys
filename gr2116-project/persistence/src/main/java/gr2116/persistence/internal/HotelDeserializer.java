714dd9eaf167f68d4b80c6ef6b51b8f669859e65
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-03 13:31:55 +0100

    #91 remove unused imports

--- gr2116-project/persistence/src/main/java/gr2116/persistence/internal/HotelDeserializer.java
+++ gr2116-project/persistence/src/main/java/gr2116/persistence/internal/HotelDeserializer.java
@@ -11,9 +11,7 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.NumericNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 
 import gr2116.core.Hotel;
 import gr2116.core.HotelRoom;
