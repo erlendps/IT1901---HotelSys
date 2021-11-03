d8806861069a9b6da365bfaf59fa838ccf4a0520
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2021-11-03 13:32:28 +0100

    #98 add finished HotelPersistence class that takes care of saving/loading

--- gr2116-project/persistence/src/main/java/gr2116/persistence/HotelPersistence.java
+++ gr2116-project/persistence/src/main/java/gr2116/persistence/HotelPersistence.java
@@ -1,8 +1,8 @@
 package gr2116.persistence;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
+import gr2116.core.Hotel;
 import gr2116.persistence.internal.HotelModule;
 
 import java.io.FileReader;
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
@@ -19,6 +18,9 @@ import java.nio.file.Paths;
  */
 public class HotelPersistence {
   private ObjectMapper mapper;
+  private static final String DATA_FOLDER
+      = Paths.get(".").toAbsolutePath()
+      .normalize().getParent().getParent().resolve("data").toString();
 
   /**
    * Constructor, creates an object mapper with our custom HotelModule.
@@ -45,7 +47,69 @@ public class HotelPersistence {
     return new ObjectMapper().registerModule(createHotelModule());
   }
 
-  
+  /**
+   * Helper method that uses the ObjectMapper to return a Hotel object from json.
+   *
+   * @param reader the IOstream used.
+   *
+   * @return Hotel object
+   *
+   * @throws IOException if something went wrong with I/O
+   */
+  private final Hotel readHotel(Reader reader) throws IOException {
+    return mapper.readValue(reader, Hotel.class);
+  }
 
+  /**
+   * Helper method that used ObjectMapper to write a Hotel object to json with
+   * pretty print.
+   *
+   * @param hotel the Hotel to be written
+   * @param writer the I/O stream to be used.
+   *
+   * @throws IOException if something went wrong with I/O
+   */
+  private final void writeHotel(Hotel hotel, Writer writer) throws IOException {
+    mapper.writerWithDefaultPrettyPrinter().writeValue(writer, hotel);
+  }
+
+  /**
+   * Loads from file a Hotel object with the given prefix.
+   *
+   * @param prefix the prefix of the file, eg "data" or "test"
+   *
+   * @return Hotel object
+   *
+   * @throws IOException if something went wrong with I/O
+   */
+  public final Hotel loadHotel(String prefix) throws IOException {
+    if (prefix == null) {
+      throw new NullPointerException("Prefix is null.");
+    }
+    try (Reader reader = new FileReader(
+        Paths.get(DATA_FOLDER, prefix + "Hotel.json").toFile(),
+        StandardCharsets.UTF_8)) {
+      return readHotel(reader);
+    }
+  }
+
+  /**
+   * Saves a Hotel object to file with the given prefix.
+   *
+   * @param hotel the Hotel to be saved
+   * @param prefix the prefix of the file
+   *
+   * @throws IOException if something went wrong with I/O
+   */
+  public final void saveHotel(Hotel hotel, String prefix) throws IOException {
+    if (prefix == null) {
+      throw new NullPointerException("Prefix is null.");
+    }
+    try (Writer writer = new FileWriter(
+        Paths.get(DATA_FOLDER, prefix + "Hotel.json").toFile(),
+        StandardCharsets.UTF_8)) {
+      writeHotel(hotel, writer);
+    }
+  }
 
 }
