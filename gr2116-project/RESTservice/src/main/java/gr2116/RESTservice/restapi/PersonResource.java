663d5c92fad13b35b8761fab7a4de52c06fed40c
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-11-10 15:08:57 +0000

    #120 Added getPerson to PersonResource

--- gr2116-project/RESTservice/src/main/java/gr2116/RESTservice/restapi/PersonResource.java
+++ gr2116-project/RESTservice/src/main/java/gr2116/RESTservice/restapi/PersonResource.java
@@ -5,6 +5,7 @@ import gr2116.core.Hotel;
 import gr2116.core.Reservation;
 import gr2116.persistence.HotelPersistence;
 import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
@@ -13,23 +14,45 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 
+/**
+ * Used for all requests referring to a Person by name.
+ */
+
 public class PersonResource {
   private static final Logger LOG = LoggerFactory.getLogger(RoomResource.class);
 
-  private final String usename;
+  private final String username;
   private final Person person;
   private final Hotel hotel;
 
   @Context
   private HotelPersistence hotelPersistence;
 
-    
+  /**
+  * Initializes PersonResource. Each method wil check and use what is needed.
+  *
+  * @param username the name of the person, needed to add a new person
+  * @param person the person, needed for most request
+  * @param hotel the hotel, needed for put
+  */
   public PersonResource(String username, Person person, Hotel hotel) {
     this.person = person;
-    this.usename = username;
+    this.username = username;
     this.hotel = hotel;
   }
 
+
+  /**
+  * Gets the corresponding person
+  *
+  * @return the corresponding person
+  */
+  @GET
+  public Person getPerson(){
+    LOG.debug("getPerson({})",username);
+    return this.person;
+  }
+  
   private void autoSaveHotel() {
     if (hotelPersistence != null) {
       try {
@@ -45,7 +68,7 @@ public class PersonResource {
   public void addPerson(String name) {
     LOG.debug("addPerson({})", name);
     Person person = new Person(name);
-    person.setUsername(usename);
+    person.setUsername(username);
     hotel.addPerson(person);
     autoSaveHotel();
   }
