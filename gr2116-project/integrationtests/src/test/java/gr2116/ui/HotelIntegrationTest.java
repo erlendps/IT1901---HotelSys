27eeced233a8ce8b4ae2fa6b189419a6e4674853
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-11-12 10:15:25 +0000

    #128 Set stage in  HotelintegrationTest

--- gr2116-project/integrationtests/src/test/java/gr2116/ui/HotelIntegrationTest.java
+++ gr2116-project/integrationtests/src/test/java/gr2116/ui/HotelIntegrationTest.java
@@ -1,21 +1,34 @@
-package todolist.ui;
+package hotel.ui;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.api.FxAssert;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.ApplicationTest;
+import gr2116.ui.controller.AppController;
 
 public class HotelIntegrtionTest extends ApplicationTest {
+
+    AppController appController;
+
+    @Start
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("App.fxml"));
+        Parent parent = fxmlLoader.load();
+        appController = (AppController) fxmlLoader.getController();
+        appController.setPrefix("test");
+        appController.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+  
+
 }
 
