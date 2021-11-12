f680050846277f147f8c8b1152b1fe220156bf91
Silje Birkeland Eriksen <siljberi@stud.ntnu.no>
2021-11-12 10:02:57 +0000

    #128 Set up HotelintegrationTest

---
+++ gr2116-project/integrationtests/src/test/java/gr2116/ui/HotelIntegrationTest.java
@@ -0,0 +1,21 @@
+package todolist.ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class HotelIntegrtionTest extends ApplicationTest {
+}
+
