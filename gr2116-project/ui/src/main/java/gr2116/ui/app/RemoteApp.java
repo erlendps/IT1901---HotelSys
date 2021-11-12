27c5c11fd7593486b963a8e9f38a4d66fd1f867f
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2021-11-12 11:19:25 +0100

    #126 add RemoteApp that runs a app using the rest api

---
+++ gr2116-project/ui/src/main/java/gr2116/ui/app/RemoteApp.java
@@ -0,0 +1,32 @@
+package gr2116.ui.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RemoteApp extends Application {
+  
+  /**
+   * Start the app, load FXML and show scene.
+   */
+  @Override
+  public final void start(final Stage stage) throws Exception {
+    System.out.println("Running RemoteApp.");
+    Parent parent = FXMLLoader.load(
+        getClass().getClassLoader().getResource("RemoteApp.fxml"));
+    stage.setScene(new Scene(parent));
+    stage.show();
+  }
+
+  /**
+   * Main method to launch app.
+   *
+   * @param args Main method needs to have these
+   */
+  public static void main(final String[] args) {
+    launch(RemoteApp.class, args);
+  }
+
+}
