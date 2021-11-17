e29d569a7fcec37ceda382c9ac8f283c0008b5d0
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2021-11-17 13:24:07 +0100

    #128 add initial IT, it has very simple tests

--- gr2116-project/integrationtests/src/test/java/gr2116/ui/HotelIT.java
+++ gr2116-project/integrationtests/src/test/java/gr2116/ui/HotelIT.java
@@ -1,8 +1,9 @@
 package gr2116.ui;
 
+import java.io.File;
 import java.io.IOException;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
@@ -17,9 +18,10 @@ import gr2116.ui.controller.AppController;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
 import gr2116.persistence.HotelPersistence;
 import gr2116.ui.access.RemoteHotelAccess;
-
 
 public class HotelIT extends ApplicationTest {
 
@@ -31,7 +33,7 @@ public class HotelIT extends ApplicationTest {
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("AppIT.fxml"));
     Parent parent = fxmlLoader.load();
     appController = (AppController) fxmlLoader.getController();
-    appController.setPrefix("test");
+    appController.setPrefix("testIt");
     stage.setScene(new Scene(parent));
     stage.show();
   }
@@ -41,12 +43,26 @@ public class HotelIT extends ApplicationTest {
     URI baseUri = new URI("http://localhost:8080/rest/hotel");
     System.out.println("Base Hotel URI: " + baseUri);
     appController.setHotelAccess(new RemoteHotelAccess(appController.getHotelPersistence(), baseUri));
-    appController.load();
   }
 
-    @Test
-    public void testController_initial(){
-        assertNotNull(this.appController);
-    }
-}
 
+  @Test
+  public void testController_initial() {
+    assertNotNull(this.appController);
+  }
+
+  @Test
+  public void testLogin() {
+    FxAssert.verifyThat("#signInButton", LabeledMatchers.hasText("Sign in"));
+    clickOn("#usernameTextField").write("tom");
+    clickOn("#signInButton");
+    FxAssert.verifyThat("#usernameLabel", LabeledMatchers.hasText("tom"));
+  }
+
+  @AfterAll
+  public static void cleanUp() {
+    File file = Paths.get(System.getProperty("user.home")
+        , "HotelSys", "testItHotel.json").toFile();
+    file.delete();
+  }
+}
