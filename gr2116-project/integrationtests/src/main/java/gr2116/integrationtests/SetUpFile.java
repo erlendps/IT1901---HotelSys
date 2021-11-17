29e07933aa095bd6c5629d1418539a8a5b87fe3b
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2021-11-17 13:22:15 +0100

    #128 add SetUpFile that sets up the file used by webserver in integration test. Added a execution in the pom

---
+++ gr2116-project/integrationtests/src/main/java/gr2116/integrationtests/SetUpFile.java
@@ -0,0 +1,66 @@
+package gr2116.integrationtests;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import gr2116.core.Amenity;
+import gr2116.core.Hotel;
+import gr2116.core.HotelRoom;
+import gr2116.core.HotelRoomType;
+import gr2116.core.Person;
+import gr2116.persistence.HotelPersistence;
+
+/**
+ * Helper class that sets up and saves the file that will be used by
+ * jetty when running integration tests.
+ */
+public class SetUpFile {
+  public static void main(String[] args) {
+    HotelPersistence hotelPersistence = new HotelPersistence("testIt");
+    // set up data
+    Person rick = new Person("Richard");
+    Person kyle = new Person("Kyllard");
+    Person tom = new Person("Tom");
+    rick.setUsername("richard");
+    kyle.setUsername("kyle");
+    tom.setUsername("tom");
+
+    rick.addBalance(1000);
+    kyle.addBalance(144);
+    tom.addBalance(1000000000);
+
+    Collection<Person> persons = new ArrayList<Person>();
+    persons.add(rick);
+    persons.add(kyle);
+    persons.add(tom);
+    
+    HotelRoom room1 = new HotelRoom(HotelRoomType.Single, 101);
+    room1.setPrice(30);
+    HotelRoom room2 = new HotelRoom(HotelRoomType.Double, 102);
+    room2.setPrice(50);
+    HotelRoom room3 = new HotelRoom(HotelRoomType.Quad, 714);
+    room3.setPrice(300);
+    
+    Collection<HotelRoom> rooms = new ArrayList<HotelRoom>();
+    rooms.add(room1);
+    rooms.add(room2);
+    rooms.add(room3);
+    
+    room1.addAmenity(Amenity.Bathtub);
+    room1.addAmenity(Amenity.Television);
+    room2.addAmenity(Amenity.Fridge);
+    room2.addAmenity(Amenity.Internet);
+    room2.addAmenity(Amenity.Shower);
+    room3.addAmenity(Amenity.KitchenFacilities);
+    room3.addAmenity(Amenity.Television);
+    room3.addAmenity(Amenity.WashingMachine);
+    room3.addAmenity(Amenity.Shower);
+
+    Hotel hotel = new Hotel(rooms, persons);
+    try {
+      hotelPersistence.saveHotel(hotel);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
