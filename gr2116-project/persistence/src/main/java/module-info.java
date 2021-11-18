3a38df974e200eb652496ffac99cb5365dcdf9e3
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2021-11-18 16:09:05 +0100

    #91 removed unused dependencies

--- gr2116-project/persistence/src/main/java/module-info.java
+++ gr2116-project/persistence/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module gr2116.persistence {
-  requires transitive org.json;
   requires transitive gr2116.core;
   requires transitive com.fasterxml.jackson.databind;
-  requires transitive org.apache.commons.io;
 
   exports gr2116.persistence;
   exports gr2116.persistence.internal;
