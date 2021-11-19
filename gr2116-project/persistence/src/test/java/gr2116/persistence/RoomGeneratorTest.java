69ada40e82e079bfd8e3daf893645db770940e29
magnetenstad <tenstad.magne@gmail.com>
2021-11-19 17:21:16 +0100

    #164 Fix RoomGenerator and RoomGeneratorTest

--- gr2116-project/persistence/src/test/java/gr2116/persistence/RoomGeneratorTest.java
+++ gr2116-project/persistence/src/test/java/gr2116/persistence/RoomGeneratorTest.java
@@ -1,23 +1,24 @@
 package gr2116.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Collection;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import gr2116.core.HotelRoom;
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
 
+/**
+ * Tests RoomGenerator.
+ */
 public class RoomGeneratorTest {
 
-    Collection<HotelRoom> rooms;
-
-    @Test
-    public void testGeneratedRooms() {
-        rooms = RoomGenerator.generateRooms(50);
-        assertEquals(50, rooms.size());
-        assert(rooms.iterator().next().getAmenities().size() > 0);
-        assert(rooms.iterator().next().getFloor() >= 1 && rooms.iterator().next().getFloor() <= 7);
+  @Test
+  public void testGeneratedRooms() {
+    Collection<HotelRoom> rooms = RoomGenerator.generateRooms(50);
+    assertEquals(50, rooms.size());
+    for (HotelRoom room : rooms) {
+      assertTrue(room.getAmenities().size() > 0);
+      assertTrue(0 < room.getFloor() && room.getFloor() < 8);
     }
-    
+  }
 }
