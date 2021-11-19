e9dea58f007bbff0262d8b7a214578361a554b23
Erlend Paulsen Skaaden <erlendps@stud.ntnu.no>
2021-11-19 17:42:37 +0100

    #157 add tests for HotelService that tests PUT and DELETE

--- gr2116-project/RESTservice/src/test/java/gr2116/restapi/HotelServiceTest.java
+++ gr2116-project/RESTservice/src/test/java/gr2116/restapi/HotelServiceTest.java
@@ -6,18 +6,26 @@ import static org.junit.jupiter.api.Assertions.fail;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gr2116.RESTservice.restapi.HotelService;
-import gr2116.RESTservice.restserver.HotelConfig;
+import gr2116.RESTservice.restserver.HotelITConfig;
 import gr2116.RESTservice.restserver.HotelModuleObjectMapperProvider;
+import gr2116.core.Amenity;
 import gr2116.core.Hotel;
 import gr2116.core.HotelRoom;
 import gr2116.core.HotelRoomType;
+import gr2116.core.Person;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.Collection;
+
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,7 +38,7 @@ public class HotelServiceTest extends JerseyTest {
 
   @Override
   protected ResourceConfig configure() {
-    final HotelConfig config = new HotelConfig();
+    final HotelITConfig config = new HotelITConfig();
     enable(TestProperties.LOG_TRAFFIC);
     enable(TestProperties.DUMP_ENTITY);
     config.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, "WARNING");
@@ -66,11 +74,10 @@ public class HotelServiceTest extends JerseyTest {
     } catch (JsonProcessingException e) {
       fail(e.getMessage());
     }
-
   }
 
   @Test
-  public void testGetRoom() {
+  public void testGetPutAndDeleteRoom() {
     Response hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
         .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
         .get();
@@ -97,10 +104,98 @@ public class HotelServiceTest extends JerseyTest {
       assertEquals(someRoomType, receivedRoom.getRoomType());
       assertEquals(someRoomAmenities, receivedRoom.getAmenities());
 
+      // putting
+      HotelRoom room = new HotelRoom(902);
+      room.setPrice(50);
+      room.addAmenity(Amenity.Bathtub);
+      
+      String json = mapper.writeValueAsString(room);
+
+      Response putResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/rooms/902")
+          .request()
+          .put(Entity.json(json));
+      assertEquals(200, putResponse.getStatus());
+      hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      hotel = mapper.readValue(hotelResponse.readEntity(String.class), Hotel.class);
+      assertEquals(31, hotel.getRooms().size());
+
+      roomResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/rooms/902")
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      assertEquals(200, roomResponse.getStatus());
+      receivedRoom = mapper.readValue(roomResponse.readEntity(String.class), HotelRoom.class);
+      assertEquals(room, receivedRoom);
+
+      Response deleteResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/rooms/902")
+          .request()
+          .delete();
+      assertEquals(200, deleteResponse.getStatus());
+      hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      hotel = mapper.readValue(hotelResponse.readEntity(String.class), Hotel.class);
+      assertEquals(30, hotel.getRooms().size());
     } catch (JsonProcessingException e) {
       fail(e.getMessage());
     }
   }
 
+  @Test
+  public void testPutGetAndDeletePerson() {
+    Response hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+        .get();
+    assertEquals(200, hotelResponse.getStatus());
+    Person person = new Person("johnd");
+    person.setFirstName("John");
+    person.setLastName("Doe");
+    person.setPassword("gaming");
+    person.addBalance(100);
+    try {
+      String json = mapper.writeValueAsString(person);
+      Response putResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/person/johnd")
+          .request()
+          .put(Entity.json(json));
+      assertEquals(200, putResponse.getStatus());
+      hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      assertEquals(200, hotelResponse.getStatus());
+      Hotel hotel = mapper.readValue(hotelResponse.readEntity(String.class), Hotel.class);
+      assertEquals(1, hotel.getPersons().size());
+      Response personResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/person/johnd")
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      assertEquals(200, personResponse.getStatus());
+      Person readPerson = mapper.readValue(personResponse.readEntity(String.class), Person.class);
+      assertEquals(person, readPerson);
+      
+      Response deleteResponse = 
+          target(HotelService.HOTEL_SERVICE_PATH + "/person/johnd")
+          .request()
+          .delete();
+      assertEquals(200, deleteResponse.getStatus());
+      hotelResponse = target(HotelService.HOTEL_SERVICE_PATH)
+          .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+          .get();
+      assertEquals(200, hotelResponse.getStatus());
+      hotel = mapper.readValue(hotelResponse.readEntity(String.class), Hotel.class);
+      assertEquals(0, hotel.getPersons().size());
+    } catch (JsonProcessingException e) {
+      fail(e.getMessage());
+    }
+  }
 
+  @AfterAll
+  public static void cleanup() {
+    File file = Paths.get(System.getProperty("user.home"), "HotelSys", "testItHotel.json").toFile();
+    file.delete();
+  }
 }
